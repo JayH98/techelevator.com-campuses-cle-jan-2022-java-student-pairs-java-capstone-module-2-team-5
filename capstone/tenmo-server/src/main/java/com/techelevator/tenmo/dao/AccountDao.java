74ad81f8545afffe0aa9979eef17fbced914afa1
Jay Hill <jeleah07@gmail.com>
2022-03-09 21:45:06 -0500

    Submitting AccountDao lines 7-14 comments

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -3,4 +3,14 @@ package com.techelevator.tenmo.dao;
 public interface AccountDao {
 
     //this is random text
+
+    //i don't know
+    //this is weird
+    //trying to see merge confilct
+    //but not really
+    //i don't know
+    //this is weird
+    //trying to see merge confilct
+    //but not really
+
 }
