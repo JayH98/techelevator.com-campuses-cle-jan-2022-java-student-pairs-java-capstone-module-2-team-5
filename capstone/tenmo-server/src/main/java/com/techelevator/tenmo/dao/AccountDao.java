86104a430fceadd0ba3d28ea7f9e91724ed2e331
Jay Hill <jeleah07@gmail.com>
2022-03-10 17:27:29 -0500

    Troubleshooting in VS code to make project go forward expirement

--- capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -4,18 +4,6 @@ import com.techelevator.tenmo.model.User;
 
 public interface AccountDao {
 
-    //this is random text
-
-    //i don't know
-    //this is weird
-    //trying to see merge confilct
-    //but not really
-    //i don't know
-    //this is weird
-    //trying to see merge confilct
-    //but not really
-
-
 
 
 
