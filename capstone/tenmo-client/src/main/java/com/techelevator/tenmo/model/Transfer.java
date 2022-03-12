e34dca24e55de63c67c97b3553efbba7a785455c
Angel Scott <angel.v.scott@gmail.com>
2022-03-11 21:05:43 -0500

    Trying to merge for the third time

---
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -0,0 +1,78 @@
+package com.techelevator.tenmo.model;
+
+public class Transfer {
+
+    private int transferId;
+    private int transferTypeId;
+    private int transferStatusId;
+    private int accountFromId;
+    private int accountToId;
+    private String accountFromUsername;
+    private String accountToUsername;
+    private double amount;
+
+
+    public int getTransferId() {
+        return transferId;
+    }
+
+    public void setTransferId(int transferId) {
+        this.transferId = transferId;
+    }
+
+    public int getTransferTypeId() {
+        return transferTypeId;
+    }
+
+    public void setTransferTypeId(int transferTypeId) {
+        this.transferTypeId = transferTypeId;
+    }
+
+    public int getTransferStatusId() {
+        return transferStatusId;
+    }
+
+    public void setTransferStatusId(int transferStatusId) {
+        this.transferStatusId = transferStatusId;
+    }
+
+    public int getAccountFromId() {
+        return accountFromId;
+    }
+
+    public void setAccountFromId(int accountFromId) {
+        this.accountFromId = accountFromId;
+    }
+
+    public int getAccountToID() {
+        return accountToId;
+    }
+
+    public void setAccountToId(int accountToID) {
+        this.accountToId = accountToID;
+    }
+
+    public String getAccountFromUsername() {
+        return accountFromUsername;
+    }
+
+    public void setAccountFromUsername(String accountFromUsername) {
+        this.accountFromUsername = accountFromUsername;
+    }
+
+    public String getAccountToUsername() {
+        return accountToUsername;
+    }
+
+    public void setAccountToUsername(String accountToUsername) {
+        this.accountToUsername = accountToUsername;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
